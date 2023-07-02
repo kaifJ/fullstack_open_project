d0c1a7e0bc87b17a2a9fd4085d9a4287bd9e72ef
kaifJ <kaif.jamadar.in@gmail.com>
2023-07-02 13:24:15 +0300

    backend: update env variables

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,7 +4,9 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
 const PORT = process.env.PORT
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.NODE_ENV === 'test' ?
+    'testjwtsecret':
+    process.env.JWT_SECRET
 
 module.exports = {
     MONGODB_URI,
