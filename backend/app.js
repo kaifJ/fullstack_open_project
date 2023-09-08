48cdd96388d5ff0e53b4d8f510d52fa3f30562aa
kaifJ <kaif.jamadar.in@gmail.com>
2023-09-08 22:28:24 +0300

    deploy: fix deploy bugs
    - serve static files from backend

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,8 @@ mongoose.connect(mongoUrl).then(() => {
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('*', express.static(path.join(__dirname, 'uploads')))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use(express.static('build'))
 
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
